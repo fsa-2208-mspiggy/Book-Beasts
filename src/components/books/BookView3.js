eff025d7dcd4fda174ab3f96b921b54563b86334
Brandon Lowe <BLoweCLE@gmail.com>
2022-10-25 22:41:45 -0500

    Fixed font-size for template 3

--- src/components/books/BookView3.js
+++ src/components/books/BookView3.js
@@ -6,7 +6,7 @@ const BookView3 = React.forwardRef(({page}, ref) => {
                 <div className="view-small-image-page">
                     {page.page.image ? (<img className="view-page-image" src={page.page.image}/>) : <div className="filler-image"></div>}
                 </div>
-                <div className="view-small-text-page" id="view-pageText">
+                <div className="view-small-text-page-3" id="view-pageText">
                     <p>{page.page.content}</p>
                 </div>
                 {page.pageNumber ? (<div className="view-page-number">Page {page.pageNumber}</div>) : (null)}
